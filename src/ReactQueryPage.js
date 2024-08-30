43e1a6e6a41737cf6b15d26f281cc7cee4514961
MayHyeyeonKim <hkim7963@gmail.com>
2024-08-29 19:02:03 -0700

    queryData, refetch

--- src/ReactQueryPage.js
+++ src/ReactQueryPage.js
@@ -2,16 +2,19 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios";
 
 const ReactQueryPage = () => {
-    const fetchPost = ()=>{return axios.get('http://localhost:3004/posts')}
+    const fetchPost = (queryData)=>{
+        // console.log("queryData: ", queryData)
+        const id = queryData.queryKey[1]
+        return axios.get(`http://localhost:3004/posts/${id}`)}
     //ReactQueryPage가 마운트되자마자 useQuery()가 실행이 되는구나.
     const { isLoading, data, isError, error, refetch } = useQuery({
-        queryKey:['posts'],
+        queryKey:['posts', 1], //unique!
         queryFn: fetchPost,
         retry: 1,
         select: (data)=>{
             return data.data //data객체안에 뭐가 잔뜩 들어있잖아? 그 중에서 data.data만 리턴하는 것임
         },
-        enabled: false //초기에 호출되지 않음 기본값 true
+        // enabled: false //초기에 호출되지 않음 기본값 true
         // refetchInterval: 3000, //3초마다 api호출
         // refetchOnMount: false, //컴포넌트가 마운트될 때 자동으로 데이터를 다시 가져올지 여부를 결정, 기본값 true
         // refetchOnWindowFocus: true, //브라우저 창에 초점이 돌아올 때 데이터를 다시 가져올지 여부를 결정, 기본값 true
@@ -36,9 +39,10 @@ const ReactQueryPage = () => {
             {error.message}
         </h1>
     }
-    return <div>{data?.map((item)=>(
+    return <div>
+    {/* {data?.map((item)=>(
         <div>{item.title}</div>
-    ))}
+    ))} */}
     <button onClick={refetch}>post리스트 다시 들고오기</button>
     </div>
 
